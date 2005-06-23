83de4319947eea3a6e60aa2b6b5dc359abc57726
Paul Gilbert <pgilbert@bank-banque-canada.ca>
2005-06-23 00:00:00 +0000

    version 2005.6-1

--- R/00First.R
+++
@@ -1,7 +0,0 @@
-.First.lib <- function(library,section){
-  # This package is data only, there is no real code. To work with the data
-  # the ts and tframe packages may be useful, but are not strickly necesssary.
-  #if(!require("ts")) warning("This package requires the ts package.")
-  #if(!require("tframe"))  warning("This package requires the tframe package.")
-  invisible()
-}
