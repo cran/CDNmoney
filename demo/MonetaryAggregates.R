0f31411794d813f0403d8601543e03c47dbb5355
Paul Gilbert <pgilbert@bank-banque-canada.ca>
2007-06-07 00:00:00 +0000

    version 2007.6-1

--- demo/MonetaryAggregates.R
+++ demo/MonetaryAggregates.R
@@ -1,34 +1,6 @@
-data("CanadianMoneyData", package="CDNmoney")
+data("CanadianMoneyData.asof.3Mar2006", package="CDNmoney")
 
 if(!require("tframe")) stop("This test requires the tframe package.")
-  # in S series must be the same length to add
-if (!is.R()) {
-  cat("################ Calculations of truncated versions for S\n")
-  MB2001 <- tfwindow(MB2001, start=c(1981, 11), end=c(2002,6)) 
-  MB486  <- tfwindow(MB486, start=c(1981, 11), end=c(2002,6)) 
-  MB487p <- tfwindow(MB487p, start=c(1981, 11), end=c(2002,6)) 
-  TMLinterbank <- tfwindow(TMLinterbank, start=c(1981, 11), end=c(2002,6)) 
-  float <- tfwindow(float, start=c(1981, 11), end=c(2002,6)) 
-  MB452 <- tfwindow(MB452, start=c(1981, 11), end=c(2002,6)) 
-  MB472 <- tfwindow(MB472, start=c(1981, 11), end=c(2002,6)) 
-  NonbankCheq <- tfwindow(NonbankCheq, start=c(1981, 11), end=c(2002,6)) 
-  CUadj <- tfwindow(CUadj, start=c(1981, 11), end=c(2002,6)) 
-  MB453 <- tfwindow(MB453, start=c(1981, 11), end=c(2002,6)) 
-  MB473 <- tfwindow(MB473, start=c(1981, 11), end=c(2002,6)) 
-  MB473adj <- tfwindow(MB473adj, start=c(1981, 11), end=c(2002,6)) 
-  NonbankNonCheq <- tfwindow(NonbankNonCheq, start=c(1981, 11), end=c(2002,6)) 
-  NonbankTerm    <- tfwindow(NonbankTerm,    start=c(1981, 11), end=c(2002,6)) 
-  MB2046 <- tfwindow(MB2046, start=c(1981, 11), end=c(2002,6)) 
-  MB2047 <- tfwindow(MB2047, start=c(1981, 11), end=c(2002,6)) 
-  MB2048 <- tfwindow(MB2048, start=c(1981, 11), end=c(2002,6)) 
-  MB2057 <- tfwindow(MB2057, start=c(1981, 11), end=c(2002,6)) 
-  MB2058 <- tfwindow(MB2058, start=c(1981, 11), end=c(2002,6)) 
-  MB475 <- tfwindow(MB475, start=c(1981, 11), end=c(2002,6)) 
-  MB482 <- tfwindow(MB482, start=c(1981, 11), end=c(2002,6))
-  M1real <- tfwindow(M1real, start=c(1981, 11), end=c(2002,6))
-  M1total <- tfwindow(M1total, start=c(1981, 11), end=c(2002,6))
-  M1PerCapita<- tfwindow(M1PerCapita, start=c(1981, 11), end=c(2002,6))
-  }
   
 cat("################ Calculations to get monetary aggregates\n")
 
